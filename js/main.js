251c20369da459eaedd6cb2059b92fa3b29d18a0
Alex Fryer <afryer@alumni.stanford.edu>
2019-11-05 07:07:06 -0800

    initial style commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function navScrollListener() {
+  let $nav = $("nav");
+  $nav.toggleClass('scrolled', $(document).scrollTop() > $nav.height());
+  $(document).scroll(function () { 
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+};
+
+function linkClickListener() {
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      }, 500, 'linear'
+    )
+  })
+}
+
+function init() {
+  navScrollListener();
+  linkClickListener();
+}
+
+$(init)
