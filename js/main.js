60e89edddf43d637eb068adef6a0830d7074406f
Alex Fryer <alex.jeffrey.fryer@gmail.com>
2020-07-23 16:35:03 -0700

    updated about section

--- js/main.js
+++ js/main.js
@@ -19,44 +19,9 @@ function linkClickListener() {
   });
 }
 
-function particleBackgrounds() {
-  Particles.init({
-    // normal options
-    selector: '.background',
-    maxParticles: 450,
-    connectParticles: true,
-    color: '#48F2E3',
-    // options for breakpoints
-    responsive: [
-      {
-        breakpoint: 768,
-        options: {
-          maxParticles: 200,
-          color: '#48F2E3',
-          connectParticles: true,
-        },
-      },
-      {
-        breakpoint: 425,
-        options: {
-          maxParticles: 100,
-          connectParticles: true,
-        },
-      },
-      {
-        breakpoint: 320,
-        options: {
-          maxParticles: 0, // disables particles.js
-        },
-      },
-    ],
-  });
-}
-
 function init() {
   navScrollListener();
   linkClickListener();
-  //particleBackgrounds();
 }
 
 $(init);
